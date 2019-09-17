e37f5b3ca8838580736507e69a7c86959ef85432
Hemberth Medina <MedinaHemberth22@gmail.com>
2019-09-17 09:50:03 -0400

    laravel passport

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+// Route::post('login', 'API\UserController@login');
+// Route::post('register', 'API\UserController@register');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
