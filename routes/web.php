3b190ff36e9ccf2db162e595fa817d541accb521
Hemberth Medina <Medinahemberth22@gmail.com>
2019-11-22 18:20:30 -0400

    recepcion

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::get('/prueba', function() {
     return view('welcome');
 });
 
@@ -28,15 +27,13 @@ Route::group(['middleware' => 'auth'], function (){
     Route::post('search/doctor/schedule','DoctorController@search_schedule')->name('search.schedule');
 
     Route::group(['middleware' => ['role:recepcion']], function () {
-        Route::get('cite', 'CitaController@index')->name('reservation.index');
+        Route::get('citas', 'CitaController@index')->name('citas.index');
         Route::get('cite/create','CitaController@create')->name('reservations.create');
-        Route::get('cite/{cite}','CitaController@edit')->name('reservation.edit');
-        Route::post('search/patient','CitaController@search_patient')->name('search.patient');
-        Route::post('cite','CitaController@store')->name('reservation.store');
+        Route::get('cite/edit/{cite}','CitaController@edit')->name('reservation.edit');
+        Route::post('search/reception/patient','CitaController@search_patient')->name('search.patient');
+        Route::post('cite/store','CitaController@store')->name('reservation.store');
         Route::post('cite/status', 'CitaController@status')->name('reservation.status');
     });
-
-
 
      //======================= rutas para el usuario ckeckin ====================
     Route::group(['middleware' => ['role:IN']], function () {
