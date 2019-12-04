21e070c209b61c2f4629b65f6287c38182497476
Yefferson Mujica <yemujica@JAKSOLUCIONES.LOCAL>
2019-12-04 12:53:49 -0400

    recipes casi listo

--- resources/views/dashboard/checkout/print_recipe.blade.php
+++ resources/views/dashboard/checkout/print_recipe.blade.php
@@ -28,46 +28,7 @@ header {
   padding: 10px 0;
   /* margin-bottom: 10px; */
   margin-top: -25px;
-}
-
-#logo img {
-  width: 120px;
-}
-
-h1 {
-  border-top: 1px solid  #000000;
-  border-bottom: 1px solid  #000000;
-  color: #FFFFFF;
-  font-size: 2.4em;
-  line-height: 1.4em;
-  font-weight: normal;
-  text-align: center;
-  margin: 0 0 20px 0;
-  background: #00ad88;
-}
-
-#project {
-  /* float: left; */
-}
-
-#project span {
-  color: #000000;
-  text-align: center;
-  width: 52px;
-  margin-right: 10px;
-  display: inline-block;
-  font-size: 0.8em;
-}
-
-#company {
-  /* float: right; */
-  /* text-align: center; */
-  color: #000000;
-}
-
-#project div,
-#company div {
-  white-space: nowrap;        
+  width: 100%;
 }
 
 table {
@@ -75,6 +36,7 @@ table {
   border-collapse: collapse;
   border-spacing: 0;
   margin-bottom: 20px;
+  margin-top: -20px;
   position: absolute;
   left: 0;
 }
@@ -85,19 +47,19 @@ table tr:nth-child(2n-1) td {
 
 table th,
 table td {
-  text-align: center;
+  text-align: justify;
 }
 
 table th {
   padding: 5px 20px;
-  border-bottom: 1px solid #000000;
+  border-top: 1px solid #248b40;
   white-space: nowrap;        
   font-weight: normal;
 }
 
 table .service,
 table .desc {
-  text-align: center;
+  text-align: justify;
   color: #000000;
 }
 
@@ -108,9 +70,12 @@ table td {
 
 table td.service{
   vertical-align: center;
+  text-align: justify;
 }
 table td.desc {
   vertical-align: center;
+  text-align: justify;
+  height: 580px;
   border-left: #a1a1a1 dashed 2px;
 }
 
@@ -120,12 +85,12 @@ table td.desc {
 }
 
 footer {
-  color: #5D6975;
+  color: #000000;
   width: 100%;
   height: 30px;
   position: absolute;
   bottom: 0;
-  border-top: 1px solid #C1CED9;
+  border-top: 1px solid #248b40;
   padding: 8px 0;
   text-align: center;
 }
@@ -150,147 +115,26 @@ footer {
   margin-top:60px;
 }
 
-.cabecera{
-  width: 50%;
-  height: 100px;
+.encabezado{
+  width: 1040px;
+  margin-top: 10px;
 }
 
-.img{
-  width: 200px;
-  max-width: 35%;
-  float: left;
-  margin-left: -15px
+.footer{
+margin-left: -550px;
+font-size: 15px;
 }
 
-.sociales{
-  width: 200px;
-  max-width: 35%;
-  float: left;
-  margin-left: -15px
+.footer2{
+margin-left: 550px;
+font-size: 15px;
+margin-top: -40px
 }
-
-.sociales ul{
-list-style: none;
-}
-
-.img img{
-  width: 100%;
-}
-
-.fila ul{
-  list-style: none;
-  width: 200px;
-  max-width: 35%;
-  float: left;
-  margin-left: -50px;
-  margin-top: 20px;
-}
-
-.cabecera2{
-  width: 50%;
-  height: 100px;
-}
-
-.img2{
-  width: 200px;
-  max-width: 35%;
-  float: left;
-  /* margin-left: -15px */
-}
-
-.sociales2{
-  width: 200px;
-  max-width: 35%;
-  float: left;
-  margin-left: -5px
-}
-
-.sociales2 ul{
-list-style: none;
-}
-
-.img2 img{
-  width: 100%;
-}
-
-.fila2 ul{
-  list-style: none;
-  width: 200px;
-  /* max-width: 35%; */
-  float: left;
-  margin-left: -10px;
-  /* margin-top: 20px; */
-}
-
-.logos{
-  width: 13%;
-  margin-left: -15px;
-  margin-top: 5px;
-  margin-right: 5px
-}
-
 </style>
   </head>
   <body>
     <header class="clearfix">
-      
-    <div  class="cabecera">
-      <div class="img">
-        <img src="assets/images/Imagotipo_S&F-NOV_Horizontal.svg" >
-      </div>
-        <div class="fila">
-          <ul>
-            <li style="text-align:center;">Consultorio:<br>
-              Calle Pdte. Gonzaléz #4
-            </li>
-            <li style="text-align:center;">Clinica:<br>
-              Calle El Vergel #27 C.P. 10107
-            </li>
-            <li style="text-align:center">Santo Domingo - Rep. Dom.</li>
-          </ul>
-        </div>
-        <div class="sociales">
-          <ul>
-            <li><img src="assets/images/Internet.svg" class="logos">www.syfdominacana.com</li>
-            <li><img src="assets/images/e-mail.svg" class="logos">info@syfdominicana.com</li>
-            <li><img src="assets/images/Instagram.svg" class="logos">@syfclinic.dom</li>
-            <li><img src="assets/images/Telefono.svg" class="logos">+1(786)544 43 16</li>
-          </ul>
-        </div>
-    </div>
-    
-    <div  class="cabecera2">
-        <div class="img2">
-          <img src="assets/images/Imagotipo_S&F-NOV_Horizontal.svg" >
-        </div>
-          {{-- <div class="fila2">
-            <ul>
-              <li style="text-align:center;">Consultorio:<br>
-                Calle Pdte. Gonzaléz #4
-              </li>
-              <li style="text-align:center;">Clinica:<br>
-                Calle El Vergel #27 C.P. 10107
-              </li>
-              <li style="text-align:center">Santo Domingo - Rep. Dom.</li>
-            </ul>
-          </div> --}}
-          {{-- <div class="sociales2">
-            <ul>
-              <li><img src="assets/images/Internet.svg" class="logos">www.syfdominacana.com</li>
-              <li><img src="assets/images/e-mail.svg" class="logos">info@syfdominicana.com</li>
-              <li><img src="assets/images/Instagram.svg" class="logos">@syfclinic.dom</li>
-              <li><img src="assets/images/Telefono.svg" class="logos">+1(786)544 43 16</li>
-            </ul>
-          </div> --}}
-      </div>
-        {{-- <h1>Recipe Medico</h1> --}}
-    {{-- <div id="project">
-      <span>PACIENTE:</span><span>Nombre</span> 
-      <span>DNI:</span><span>AQUI</span>  
-      <span>DIRECCIÓN:</span><span>AQUI</span> 
-      <span>EMAI:</span><span>AQUI</span> 
-      <span class="fecha">FECHA:</span><span>August 17, 2015</span> 
-    </div> --}}
+      <img src="assets/images/Encabezado_Recipe.png" class="encabezado">
     </header>
     <main>
       <img src="assets/images/Isotipo_S&F.svg" class="imgfondo">
@@ -298,8 +142,8 @@ list-style: none;
     <table>
         <thead>
         <tr>
-            <th class="service">MEDICAMENTOS</th>
-            <th class="desc">INDICACIONES</th>
+            <th class="service"><h3 style="margin-top:20px;margin-bottom:-5px">MEDICAMENTOS:</h3></th>
+            <th class="desc" style="border-left:#a1a1a1 dashed 2px"><h3 style="margin-top:20px;margin-bottom:-5px">INDICACIONES:</h3></th>
         </tr>
         </thead>
         <tbody>
@@ -323,7 +167,18 @@ list-style: none;
     </div> --}}
     </main>
     <footer>
-    Invoice was created on a computer and is valid without the signature and seal.
+      <div class="footer">
+        <span>PACIENTE:</span><span style="text-decoration:underline">Yefferson</span>
+        <span>EDAD:</span><span style="text-decoration:underline">________________</span><br>   
+        <span>DOCTOR:</span><span style="text-decoration:underline">________________</span> 
+        <span>FECHA:</span><span style="text-decoration:underline">August 17, 2015</span>
+      </div>
+      <div class="footer2">
+        <span>PACIENTE:</span><span style="text-decoration:underline">______________</span>
+        <span>EDAD:</span><span style="text-decoration:underline">________________</span><br>   
+        <span>DOCTOR:</span><span style="text-decoration:underline">________________</span> 
+        <span>FECHA:</span><span style="text-decoration:underline">August 17, 2015</span>
+      </div>
     </footer>
 </body>
 </html>
