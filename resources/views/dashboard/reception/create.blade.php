0a9af1b781014624e5788688f82ae43c19ddf987
Kenwherly Hernandez <kehernandez@JAKSOLUCIONES.LOCAL>
2019-12-11 19:07:43 -0400

    recepcion

--- resources/views/dashboard/reception/create.blade.php
+++ resources/views/dashboard/reception/create.blade.php
@@ -181,6 +181,15 @@
         headerTag: 'h2',
         bodyTag: 'section',
         transitionEffect: 'slideLeft',
+        labels: {
+            cancel: "Cancelar",
+            current: "Paso actual:",
+            pagination: "Paginación",
+            finish: "Finalizar",
+            next: "Siguiente",
+            previous: "Anterior",
+            loading: "Cargando ..."
+        },
         onInit: function(event, currentIndex) {
             setButtonWavesEffect(event);
             search();
@@ -198,9 +207,10 @@
         onFinished: function(event, currentIndex) {
             crear();
             Swal.fire({
-                title: 'Excelente!',
-                text: 'Cita Agendada Exitosamente!',
+                title: 'Cita Agendada!',
+                text: 'Click OK para cerrar!',
                 type: 'success',
+
             });
         }
     });
@@ -304,8 +314,8 @@
                 })
                 .done(function(data) {
                     Swal.fire({
-                        title: 'Excelente!',
-                        text: 'Especialidad con medicos',
+                        title: 'Realizado!',
+                        text: 'Click en OK para continuar',
                         type: 'success',
                     });
                     $('#speciality').val(data[0].id);
@@ -346,8 +356,8 @@
                     console.log('Doctores:',data);
                     console.log('Fechas disponibles de los Doctores',data.available);
                     Swal.fire({
-                        title: 'Excelente!',
-                        text: 'Medico Seleccionado',
+                        title: 'Médico seleccionado!',
+                        text: 'Click en OK para continuar',
                         type: 'success',
                     });
                     $('#doctor').val(data.employe.id);
