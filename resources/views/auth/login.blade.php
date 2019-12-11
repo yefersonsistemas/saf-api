c00de4530a9e15061f2de6fe5f4b650436f2b98c
Eduardo Castillo <eduanc2@gmail.com>
2019-12-11 12:13:14 -0400

    capa-imagen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,7 @@
     <div class="auth_left">
         <div class="card border-0">
             <div class="text-center mb-3">
-                <a class="header-brand" href="https://sinussystem.logotipomiami.com/login">
+                <a class="header-brand logo-login" href="https://sinussystem.logotipomiami.com/login">
                     <img src="{{ asset('img/Imagotipo_S&F-NOV_Vertical-01.svg') }}" alt="">
                 </a>
             </div>
