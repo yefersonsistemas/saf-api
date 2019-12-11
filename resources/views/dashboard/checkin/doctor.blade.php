9454dfdcd088f6b331a6e6fca879b6dc1ea94fd9
Hemberth Medina <Medinahemberth22@gmail.com>
2019-12-11 14:21:56 -0400

    typesurgery

--- resources/views/dashboard/checkin/doctor.blade.php
+++ resources/views/dashboard/checkin/doctor.blade.php
@@ -10,17 +10,17 @@
 @section('title','Medicos del dia')
 
 @section('content')
-<div class="col-md-12 mt-3">
-    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-        <li class="nav-item col-md-2">
-            <a class="nav-link btn-block  p-2 d-flex flex-row justify-content-center active btn btn-outline-primary m-auto" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Del dia</a>
-        </li>
-        <li class="nav-item col-md-2">
-            <a class="nav-link btn-block  p-2 d-flex flex-row justify-content-center btn btn-outline-success" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Todos</a>
-        </li>
-    </ul>
-</div>
-<div class="tab-content mx-auto" id="pills-tabContent">
+    <div class="col-md-12 mt-3">
+        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li class="nav-item col-md-2">
+                <a class="nav-link btn-block  p-2 d-flex flex-row justify-content-center active btn btn-outline-primary m-auto" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Del dia</a>
+            </li>
+            <li class="nav-item col-md-2">
+                <a class="nav-link btn-block  p-2 d-flex flex-row justify-content-center btn btn-outline-success" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Todos</a>
+            </li>
+        </ul>
+    </div>
+    <div class="tab-content mx-auto" id="pills-tabContent">
         <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
             <div class="col-lg-12">
                 <div class="table-responsive mb-4">
@@ -35,8 +35,8 @@
                                 <th>Horario</th>
                                 <th>Asistencia</th>
                             </tr>
-                            </thead>
-                            <tfoot>
+                        </thead>
+                        <tfoot>
                             <tr>
                                 <th>Foto</th>
                                 <th>Cedula</th>
@@ -46,24 +46,24 @@
                                 <th>Horario</th>
                                 <th>Asistencia</th>
                             </tr>
-                            </tfoot>
-                            <tbody>
-                                @foreach($em as $employe)
+                        </tfoot>
+                        <tbody>
+                            @foreach($em as $employe)
                                 <tr>
                                     <td>
                                         @if (!empty($employe->image->path))
-                                        <img src="{{ Storage::url($employe->image->path) }}" alt="">
+                                            <img src="{{ Storage::url($employe->image->path) }}" alt="">
                                         @else
-                                        <img src="" alt="" >
+                                            <img src="" alt="" >
                                         @endif
                                     </td>
                                     <td>{{ $employe->person->dni }}</td>
                                     <td>{{ $employe->person->name }}</td>
                                     <td>{{ $employe->person->lastname }}</td>
                                     <td>
-                                    @foreach ($employe->speciality as $item)
-                                        {{ $item->name }} <br>
-                                    @endforeach
+                                        @foreach ($employe->speciality as $item)
+                                            {{ $item->name }} <br>
+                                        @endforeach
                                     </td>
                                     <!--Ver horario del medico-->
                                     <td>
@@ -80,10 +80,9 @@
                                             <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal" data-whatever="Cancelar asistencia de: {{ $employe->person->name }} {{ $employe->person->lastname }}" data-id="{{ $employe->id }}">Cancelar</button>
                                         @endif
                                     </td>
-   
                                 </tr>
-                                @endforeach
-                            </tbody>
+                            @endforeach
+                        </tbody>
                     </table>
                 </div>
             </div>  
@@ -141,33 +140,28 @@
                 </div>
             </div> 
         </div>
-
-
-
-  
+    </div>
 
     <!-- Modal -->
     @foreach ($em as $employe) 
         <div class="modal fade" id="{{ $employe->person->type_dni }}{{ $employe->person->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header" style="text-align:center">
-                <h5 class="modal-title" id="exampleModalLabel">Horario del Doctor</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                </div>
-                <div class="modal-body">
-                        
+                <div class="modal-content">
+                    <div class="modal-header" style="text-align:center">
+                    <h5 class="modal-title" id="exampleModalLabel">Horario del Doctor</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    </div>
+                    <div class="modal-body">
                         <table class="table table-bordered table-sm" style="borde-radious: 5px">
-                                <thead class="table-info">
+                            <thead class="table-info">
                                 <tr>
                                     <th scope="col" style="text-align:center; color:black">Dias</th>
                                     <th scope="col" style="text-align:center; color:black">Turno</th>
                                 </tr>
-                                </thead>
-                                <tbody>
-                              
+                            </thead>
+                            <tbody>
                                 <tr>
                                     @foreach ($employe->schedule as $item)
                                         @if ($item->day == 'monday')
@@ -191,12 +185,11 @@
                                         @if ($item->turn ==  'tarde')
                                         <td style="text-align:center">Tarde</td>
                                         @endif
-                                          </tr>
                                     @endforeach
-                                
-                                </tbody>
-                            </table>    
-                        </div>
+                                </tr>
+                            </tbody>
+                        </table>    
+                    </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
                     </div>
@@ -206,7 +199,6 @@
     @endforeach
 
     <!--Modal de asistencia-->
-        
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
