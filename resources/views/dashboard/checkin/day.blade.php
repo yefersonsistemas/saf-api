94cde7c0313e29ec6aa935947c9acca24b1a6bec
Hemberth Medina <Medinahemberth22@gmail.com>
2019-12-11 20:52:48 -0400

    font-size

--- resources/views/dashboard/checkin/day.blade.php
+++ resources/views/dashboard/checkin/day.blade.php
@@ -56,7 +56,7 @@
                         <tbody>
                             @foreach ($day as $reservation)
                                 <tr>
-                                    <td style="text-align: center;">
+                                    <td style="text-align: center; font-size:10px;">
                                         @if (!empty($reservation->patient->image->path))
                                             <img class="rounded circle" width="150px" height="auto" src="{{ Storage::url($reservation->patient->image->path) }}" alt="">
                                         @else
