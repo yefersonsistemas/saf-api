45ceb5b5f244296a74d4797cca8f48adfc535c92
Kenwherly Hernandez <kehernandez@JAKSOLUCIONES.LOCAL>
2020-01-07 17:33:19 -0400

    director

--- resources/views/dashboard/director/type-area.blade.php
+++ resources/views/dashboard/director/type-area.blade.php
@@ -24,7 +24,7 @@
                             <div class="form-group col-lg-3 col-md-3">
                                 <label class="form-label">Foto</label>
                                 <div class=" mb-2">
-                                    <input id="image" type="file" class="dropify" name="image" data-default-file="">
+                                    <input id="image" required type="file" class="dropify" name="image" data-default-file="">
                                 </div>
                             </div>
 
