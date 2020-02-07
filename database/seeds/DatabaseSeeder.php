4b48d64539ca9fc2fbcaf48a9915afcdf4c33ac8
jefersonmujica <jefersonmujica@gmail.com>
2020-02-07 19:31:58 -0400

    actualizacion

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,59 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Schema::disableForeignKeyConstraints();
-        $this->call(RolesAndPermissionsTablesSeeders::class);
-        $this->call(HeadquartersTableSeeder::class);
-        $this->call(BranchTableSeeder::class);
-        // $this->call(PersonTableSeeder::class);
-        $this->call(ServicesTableSeeder::class);
-        $this->call(SpecialitiesTableSeeder::class);
-        // $this->call(TreatmentsTableSeeder::class);
-        $this->call(DiseasesTableSeeder::class);
-        $this->call(ProcedureTableSeeder::class); 
-        $this->call(UsersTableSeeder::class);
-        // $this->call(PositionsTableSeeder::class);
-        // $this->call(EmployeTableSeeder::class);
-        $this->call(VisitorTableSeeder::class);
-        // $this->call(PatientsTableSeeder::class);
-        // $this->call(SchedulesTableSeeder::class);
-        // $this->call(ReservationsTableSeeder::class);
-        $this->call(CitesTableSeeder::class);
-        $this->call(ConsultationTypesTableSeeder::class);
-        $this->call(ConfigurationsTableSeeder::class);
-        // $this->call(DiagnosticsTableSeeder::class);
-        // $this->call(TypeAreasTableSeeder::class);
-        // $this->call(AreasTableSeeder::class);
-        $this->call(ClassificationsTableSeeder::class);
-        $this->call(AreaAssigmentsTableSeeder::class);
-        $this->call(MedicinesTableSeeder::class);
-        // $this->call(BalancesTableSeeder::class);
-        // $this->call(TypeDoctorTableSeeder::class);
-        // $this->call(DoctorTableSeeder::class);
-        // $this->call(TypeEquipmentTableSeeder::class);
-        // $this->call(EquipmentTableSeeder::class);
-        $this->call(PaymentTableSeeder::class);
-        $this->call(TypeSupplieTableSeeder::class);     
-        $this->call(SupplieTableSeeder::class);
-        // $this->call(ClassificationSurgeryTableSeeder::class);
-        // $this->call(TypeSurgeriesTableSeeder::class);
-        $this->call(HospitalizationTableSeeder::class);
-        $this->call(SurgeriesTableSeeder::class);
-        $this->call(TypeCurrenciesTableSeeder::class);
-        $this->call(TypePaymentsTableSeeder::class);
-        $this->call(InventoriesTableSeeder::class);
-        $this->call(InventoryAreasTableSeeder::class);
-        $this->call(BillingsTableSeeder::class);
-        $this->call(IncomeTableSeeder::class);
-        $this->call(ExamTableSeeder::class);
-        $this->call(TypeCleaningTableSeeder::class);
-        $this->call(CleaningRecordTableSeeder::class);
-        $this->call(NotificationsTableSeeder::class);
-        $this->call(BudgetTableSeeder::class);
-        $this->call(AssistanceTableSeeder::class);
-        $this->call(RecipeTableSeeder::class);
-        $this->call(ItineraryTableSeeder::class);
-        $this->call(AllergyTableSeeder::class);
-        Schema::enableForeignKeyConstraints();
+        // $this->call(UsersTableSeeder::class);
     }
 }
