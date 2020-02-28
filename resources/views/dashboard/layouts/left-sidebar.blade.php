14d2123cc309c5769305735fde7a876171a247a7
Ysbelia cariazo <ysbeliacariazo@gmail.com>
2020-02-28 18:55:45 -0400

    fusion ken y yeff

--- resources/views/dashboard/layouts/left-sidebar.blade.php
+++ resources/views/dashboard/layouts/left-sidebar.blade.php
@@ -7,11 +7,11 @@
 
 <div id="left-sidebar" class="sidebar">
     <div class="container mt--20">
-        @if (Auth::user()->role('director') == 'director')
-        <h5 class="brand-name">{{ ucfirst(Auth::user()->getRoleNames()[11]) }}<a href="#" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5>
-       @elseif(Auth::user()->role('director') != 'director')
+        {{-- @if (Auth::user()->role('director') == 'director') --}}
+        {{-- <h5 class="brand-name">{{ ucfirst(Auth::user()->getRoleNames()[11]) }}<a href="#" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5> --}}
+       {{-- @elseif(Auth::user()->role('director') != 'director') --}}
         <h5 class="brand-name">{{ ucfirst(Auth::user()->getRoleNames()[0]) }}<a href="#" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a></h5>
-        @endif
+        {{-- @endif --}}
         <nav id="left-sidebar-nav" class="sidebar-nav">
             <ul class="metismenu">
                 {{-- @role('recepcion')
